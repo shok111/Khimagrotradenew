e63d5ab09ff74f6204469c607922379d450443bd
Greed1n <108575314+shok111@users.noreply.github.com>
2023-07-31 11:10:35 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("click", function (e) {
-    const targetElement = e.target;
+	const targetElement = e.target;
 
-    if (targetElement.closest('.menu-burger')) {
-        document.documentElement.classList.toggle('menu-open');
-        e.preventDefault();
-    }
+	if (targetElement.closest('.menu-icon')) {
+		document.documentElement.classList.toggle('menu-open');
+		e.preventDefault();
+	}
 
 })
